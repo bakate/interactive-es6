ebdc2429ebcde642969fbac3bd665e6620374566
Christophe Porteneuve <tdd@tddsworld.com>
2016-01-27 16:47:17 +0100

    Refactor fixture LS store

--- app/challenge-store.js
+++ app/challenge-store.js
@@ -1,22 +1,19 @@
 import store from 'store';
 
-const STORE_PREFIX = 'es6workshop=';
-
+const STORE_PREFIX = '@@es6-jst/';
 
 const fixtureToKey = (fixture) => STORE_PREFIX + fixture.split('.')[0];
 
+function skipStorage() {
+  return store.get(fixtureToKey('ignore-fixtures'));
+}
+
 export function saveChallenge(fixture, src) {
-  store.set(fixtureToKey(fixture), src);
+  if (!skipStorage()) {
+    store.set(fixtureToKey(fixture), src);
+  }
 }
 
 export function getChallenge(fixture) {
-  if (store.get(fixtureToKey('ignore-fixtures'))) {
-    return null;
-  }
-  return store.get(fixtureToKey(fixture));
+  return skipStorage() ? null : store.get(fixtureToKey(fixture));
 }
-
-export function clearChallenge(fixture) {
-  store.remove(fixtureToKey(fixture));
-}
-

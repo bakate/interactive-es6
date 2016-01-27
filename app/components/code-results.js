4c9ea7b86544b6487985019c9e3d45247307802e
Christophe Porteneuve <tdd@tddsworld.com>
2016-01-27 16:07:07 +0100

    Use randomized OK/KO badges in code results, for delight

--- app/components/code-results.js
+++ app/components/code-results.js
@@ -3,24 +3,34 @@ import React from 'react';
 const ASSERTION_ERROR_MAPS = {
   'equal': (x, y) => {
     return (
-      <span>Je m’attendais à {valueOrUndefined(y)} plutôt qu’à {valueOrUndefined(x)}</span>
+      <span>Je m’attendais à {valueOrUndefined(y)} et j’ai {valueOrUndefined(x)}</span>
     )
   },
   'resolvesTo': (_, y) => {
     return (
-      <span>La promesse aurait dû s’accomplir avec {valueOrUndefined(y)}</span>
+      <span>La promesse devrait s’accomplir avec {valueOrUndefined(y)}</span>
     )
   },
   'method missing': (err) => {
     const target = err.message.split(' ')[0]
     return (
-      <span>Je n’ai pas trouvé la fonction <code>{target}</code></span>
+      <span>Je cherchais la fonction <code>{target}</code></span>
     )
   }
 };
 
 function valueOrUndefined(x) {
   return (<code>{x == undefined ? 'undefined' : x}</code>);
+}
+
+const SOURCES = {
+  true: ['Trop fort !', '\\o/', 'Tu roxxes !', 'Legit!', 'Righteous!', 'Bogoss™', 'Joliii…', 'Bon ça…'],
+  false: ['Dommage', 'Argh !', 'Oops…', 'Damned!', 'Et m…', 'Sapristi !', 'Disgrâce !']
+}
+
+function randomReaction(passed) {
+  const source = SOURCES[passed]
+  return source[Math.floor(Math.random() * source.length)]
 }
 
 function resultToString(result) {
@@ -37,7 +47,7 @@ export default class CodeResults extends React.Component {
       <li key={JSON.stringify(result)} className={cssClass}>
         { resultToString(result) }
         <span className="badge">
-          { result.passed === true ? 'Trop fort ! ' : 'Dommage ! ' }
+          { randomReaction(result.passed === true) }
         </span>
       </li>
     )

56e2c4cdc9665fe2b80593895f21c68345528d93
Christophe Porteneuve <tdd@tddsworld.com>
2017-10-17 16:43:11 +0200

    Fix config et lien superflu fin fx fléchées

--- app/components/arrow-functions.js
+++ app/components/arrow-functions.js
@@ -1,18 +1,15 @@
-import React from 'react';
+import React from 'react'
 
-import Feature from './feature';
+import Feature from './feature'
 
-import { Link } from 'react-router';
+import { Link } from 'react-router'
 
 export default class ArrowFunctions extends React.Component {
   render() {
     return (
       <div className="challenge row">
         <Feature feature="arrow-functions" />
-        <div className="col-md-12">
-          <Link className="btn btn-primary btn-lg btn-block" to= "/promises">Promets-moi que tu vas cliquer ici</Link>
-        </div>
       </div>
-    );
+    )
   }
 }
